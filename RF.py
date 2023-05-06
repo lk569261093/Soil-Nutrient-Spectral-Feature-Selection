d0467c766882ea1fbe8a3173ace1feab0409b991
Kai Liu <59326332+lk569261093@users.noreply.github.com>
2023-05-06 17:20:02 +0800

    Update RF.py

--- RF.py
+++ RF.py
@@ -3,21 +3,21 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
-# 读取 Excel 数据
+# Read Excel data
 data = pd.read_excel('SOM.xlsx')
 X = data.iloc[:, 1:].values
 y = data.iloc[:, 0].values
 
-# 数据预处理
+# Data preprocessing
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# 使用随机森林回归器计算特征重要性
+# Calculate feature importance using a random forest regressor
 rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 rf_regressor.fit(X_scaled, y)
 importances = rf_regressor.feature_importances_
 
-# 对特征重要性进行排序，并获取前15个最重要特征的索引
+# Sort the feature importance and obtain the indices of the top 15 most important features.
 top_15_indices = np.argsort(importances)[-15:][::-1]
 
 print("Top 15 most important spectral features:")
